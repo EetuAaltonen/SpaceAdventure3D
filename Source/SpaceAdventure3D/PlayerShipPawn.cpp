33c296b92b6bf1464d2956978f3a602399e9c0f8
TheHeroOfNoOne <eetu.aaltonen98@gmail.com>
2021-11-28 22:28:25 +0200

    Add blueprint and class for player ship pawn

---
+++ Source/SpaceAdventure3D/PlayerShipPawn.cpp
@@ -0,0 +1,142 @@
+#include "PlayerShipPawn.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Camera/CameraComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Engine/World.h"
+#include "Engine/StaticMesh.h"
+
+// Sets default values
+APlayerShipPawn::APlayerShipPawn()
+{
+	struct FConstructorStatics
+	{
+		ConstructorHelpers::FObjectFinderOptional<UStaticMesh> PlaneMesh;
+		FConstructorStatics()
+			: PlaneMesh(TEXT("/Game/Flying/Meshes/UFO.UFO"))
+		{
+		}
+	};
+	static FConstructorStatics ConstructorStatics;
+
+	// Create static mesh component
+	PlaneMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PlaneMesh0"));
+	PlaneMesh->SetStaticMesh(ConstructorStatics.PlaneMesh.Get());
+	RootComponent = PlaneMesh;
+
+	// Create a spring arm component
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm0"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 300.0f; // Camera offset distance
+	SpringArm->SocketOffset = FVector(0.f, 0.f, 60.f);
+	SpringArm->bEnableCameraLag = false;
+	SpringArm->CameraLagSpeed = 15.f;
+
+	// Create camera component 
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera0"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false;
+
+	// Set movement parameters
+	Acceleration = 500.f;
+	TurnSpeed = 100.f;
+	RollSpeed = 50.f;
+	MaxForwardSpeed = 4000.f;
+	MaxReverseSpeed = -1000.f;
+	CurrentForwardSpeed = 500.f;
+
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlayerShipPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerShipPawn::Tick(float DeltaSeconds)
+{
+	const FVector LocalMove = FVector(CurrentForwardSpeed * DeltaSeconds, 0.f, 0.f);
+
+	// Move plan forwards (with sweep so we stop when we collide with things)
+	AddActorLocalOffset(LocalMove, true);
+
+	// Calculate change in rotation this frame
+	FRotator DeltaRotation(0, 0, 0);
+	DeltaRotation.Pitch = CurrentPitchSpeed * DeltaSeconds;
+	DeltaRotation.Yaw = CurrentYawSpeed * DeltaSeconds;
+	DeltaRotation.Roll = CurrentRollSpeed * DeltaSeconds;
+
+	// Rotate plane
+	AddActorLocalRotation(DeltaRotation);
+
+	// Call any parent class Tick implementation
+	Super::Tick(DeltaSeconds);
+}
+
+void APlayerShipPawn::NotifyHit(class UPrimitiveComponent* MyComp, class AActor* Other, class UPrimitiveComponent* OtherComp,
+								bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit)
+{
+	Super::NotifyHit(MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit);
+
+	// Deflect along the surface when we collide.
+	FRotator CurrentRotation = GetActorRotation();
+	SetActorRotation(FQuat::Slerp(CurrentRotation.Quaternion(), HitNormal.ToOrientationQuat(), 0.025f));
+}
+
+void APlayerShipPawn::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	// Check if PlayerInputComponent is valid (not NULL)
+	check(PlayerInputComponent);
+
+	// Bind control axis' to callback functions
+	PlayerInputComponent->BindAxis("MoveUp", this, &APlayerShipPawn::MoveUpInput);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerShipPawn::MoveRightInput);
+	PlayerInputComponent->BindAxis("Thrust", this, &APlayerShipPawn::ThrustInput);
+	PlayerInputComponent->BindAxis("Roll", this, &APlayerShipPawn::RollInput);
+	PlayerInputComponent->BindAxis("Boost", this, &APlayerShipPawn::ThrustInput);
+}
+
+void APlayerShipPawn::MoveUpInput(float Val)
+{
+	float TargetPitchSpeed = (Val * TurnSpeed * -1.f);
+	TargetPitchSpeed += (FMath::Abs(CurrentYawSpeed) * -0.2f);
+
+	CurrentPitchSpeed = FMath::FInterpTo(CurrentPitchSpeed, TargetPitchSpeed, GetWorld()->GetDeltaSeconds(), 2.f);
+}
+
+void APlayerShipPawn::MoveRightInput(float Val)
+{
+	float TargetYawSpeed = (Val * TurnSpeed);
+
+	CurrentYawSpeed = FMath::FInterpTo(CurrentYawSpeed, TargetYawSpeed, GetWorld()->GetDeltaSeconds(), 2.f);
+}
+
+void APlayerShipPawn::ThrustInput(float Val)
+{
+	float CurrentAcc = Val * Acceleration;
+	float NewForwardSpeed = CurrentForwardSpeed + (GetWorld()->GetDeltaSeconds() * CurrentAcc);
+
+	CurrentForwardSpeed = FMath::Clamp(NewForwardSpeed, MaxReverseSpeed , MaxForwardSpeed);
+}
+
+void APlayerShipPawn::RollInput(float Val)
+{
+	float TargetRollSpeed = Val * RollSpeed;
+
+	CurrentRollSpeed = FMath::FInterpTo(CurrentRollSpeed, TargetRollSpeed, GetWorld()->GetDeltaSeconds(), 2.f);
+}
+
+void APlayerShipPawn::BoostInput(float Val)
+{
+	/*bool bHasInput = !FMath::IsNearlyEqual(Val, 0.f);
+	float CurrentAcc = bHasInput ? (Val * Acceleration) : (-0.5f * Acceleration);
+	float NewForwardSpeed = CurrentForwardSpeed + (GetWorld()->GetDeltaSeconds() * CurrentAcc);
+
+	CurrentForwardSpeed = FMath::Clamp(NewForwardSpeed, MinSpeed, MaxSpeed);*/
+}
---
+++ Source/SpaceAdventure3D/PlayerShipPawn.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerShipPawn.generated.h"
+
+UCLASS(Config = Game)
+class SPACEADVENTURE3D_API APlayerShipPawn : public APawn
+{
+	GENERATED_BODY()
+
+	// StaticMesh
+	UPROPERTY(Category = Mesh, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* PlaneMesh;
+
+	// Spring arm to set camera offset
+	UPROPERTY(Category = Camera, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* SpringArm;
+
+	// Camera component
+	UPROPERTY(Category = Camera, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* Camera;
+
+public:
+	
+	// Sets default values for this pawn's properties
+	APlayerShipPawn();
+
+	// AActor overrides
+	virtual void Tick(float DeltaSeconds) override;
+	virtual void NotifyHit(class UPrimitiveComponent* MyComp, class AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
+
+protected:
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// APawn overrides
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+	// Bond axis
+	void MoveUpInput(float Val);
+
+	void MoveRightInput(float Val);
+
+	void ThrustInput(float Val);
+
+	void RollInput(float Val);
+
+	void BoostInput(float Val);
+
+private:
+
+	UPROPERTY(Category = Plane, EditAnywhere)
+	float Acceleration;
+
+	UPROPERTY(Category = Plane, EditAnywhere)
+	float RollSpeed;
+
+	UPROPERTY(Category = Plane, EditAnywhere)
+	float TurnSpeed;
+
+	UPROPERTY(Category = Pitch, EditAnywhere)
+	float MaxForwardSpeed;
+
+	UPROPERTY(Category = Yaw, EditAnywhere)
+	float MaxReverseSpeed;
+
+	UPROPERTY(Category = Pitch, EditAnywhere)
+	float MaxBoostSpeed;
+
+	float CurrentForwardSpeed;
+
+	float CurrentYawSpeed;
+
+	float CurrentPitchSpeed;
+
+	float CurrentRollSpeed;
+
+public:
+
+	FORCEINLINE class UStaticMeshComponent* GetPlaneMesh() const { return PlaneMesh; }
+
+	FORCEINLINE class USpringArmComponent* GetSpringArm() const { return SpringArm; }
+
+	FORCEINLINE class UCameraComponent* GetCamera() const { return Camera; }
+};
