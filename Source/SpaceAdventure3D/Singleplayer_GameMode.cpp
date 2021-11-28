6de6eff8ffb6f14294522cb67625f31b8d7c1be7
TheHeroOfNoOne <eetu.aaltonen98@gmail.com>
2021-11-28 22:28:58 +0200

    Add blueprint and class for singleplayer gamemode

---
+++ Source/SpaceAdventure3D/Singleplayer_GameMode.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "Singleplayer_GameMode.h"
+
---
+++ Source/SpaceAdventure3D/Singleplayer_GameMode.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "Singleplayer_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEADVENTURE3D_API ASingleplayer_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
