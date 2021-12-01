0c97586647e67d52fd7b8dbab0e4f9c193b2c050
TheHeroOfNoOne <eetu.aaltonen98@gmail.com>
2021-12-01 17:33:18 +0200

    Add blueprint and class for Landing cinematic camera

---
+++ Source/SpaceAdventure3D/LandingCineCameraActor.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CineCameraActor.h"
+#include "LandingCineCameraActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEADVENTURE3D_API ALandingCineCameraActor : public ACineCameraActor
+{
+	GENERATED_BODY()
+	
+};
